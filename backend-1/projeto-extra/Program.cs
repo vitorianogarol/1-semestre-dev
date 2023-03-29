18fe745cfecf909d27e4a019c8dd41e773b44ff2
Vitória Luzia Nogarol Lopes <44072278858@info.local>
2023-03-29 17:28:20 -0300

    back-end atividades, add extras

---
+++ backend-1/projeto-extra/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine(@$"
+----------------------
+|      Programa      |
+|       media        |
+----------------------
+");
+
+Console.WriteLine($"Informe sua n1: ");
+int n1 = int.Parse (Console.ReadLine());
+
+Console.WriteLine($"Informe sua n2: ");
+int n2 = int.Parse (Console.ReadLine());
+
+Console.WriteLine($"Informe sua n3: ");
+int n3 = int.Parse (Console.ReadLine());
+
+Console.WriteLine($"Informe sua n4: ");
+int n4 = int.Parse (Console.ReadLine());
+
+Console.WriteLine($"Informe sua n5: ");
+int n5 = int.Parse (Console.ReadLine());
+
+int media = (n1 + n2 + n3 + n4 + n5) / 5;
+
+
+Console.WriteLine($"Sua media e {media}");
+
---
+++ backend-1/projeto-extra2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine(@$"
+----------------------
+|      Programa      |
+|      quitanda      |
+----------------------
+");
+
+Console.WriteLine($"Defina o valor do kg de seu produto:");
+float valor = float.Parse (Console.ReadLine());
+
+Console.WriteLine($"Kilogramas de seu produto:");
+float kg = float.Parse (Console.ReadLine());
+
+float valorTotal = valor * kg;
+Console.WriteLine($"O valor de sua compra R${valorTotal}");
+
+
+
