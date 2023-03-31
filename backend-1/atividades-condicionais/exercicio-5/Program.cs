99eb3b74871a236f222397a9402248e59fe75b2c
Vitória Luzia Nogarol Lopes <44072278858@info.local>
2023-03-31 15:19:36 -0300

    Ativididades Condicionais

---
+++ backend-1/atividades-condicionais/exercicio-5/Program.cs
@@ -0,0 +1,21 @@
+﻿// As    maçãs custam R$ 0,30 cada se forem compradas    menos do que uma dúzia, e R$ 0,25 se forem compradas pelo menos doze. Escreva um programa que leia o número de maçãs compradas, calcule e escreva o valor    total    da compra.
+
+// float trinta = 0.3F
+Console.WriteLine($"Insira quantas maças voce pegou");
+ int maca = int.Parse (Console.ReadLine());
+
+
+if (maca >= 12 )
+{
+    float v1 = maca * 0.30f;
+    Console.WriteLine($"Total foi R${v1}");
+    
+
+}
+
+else 
+{
+    float v2 = maca * 0.25f;
+    Console.WriteLine($"Total foi de {Math.Round(v2, 2)} maças");
+}
+
---
+++ backend-1/atividades-condicionais/exercicio=6/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine($"Insira sua frequencia");
+int frequencia = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Insira sua media");
+int media = int.Parse(Console.ReadLine());
+
+if (frequencia < 75)
+{
+    Console.WriteLine($"Reprovado");
+
+}
+
+else if (media >= 3 )
+{
+    if (media > 7 ){
+    Console.WriteLine($"Aprovado");
+    }
+
+    else {
+         Console.WriteLine($"Avaliaçao de Recuperaçao");
+    }
+}
+
+else {
+     Console.WriteLine($"Reprovado");
+}
