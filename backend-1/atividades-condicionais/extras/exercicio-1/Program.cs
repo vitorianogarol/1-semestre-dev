934ed4a923c19e5e1d9fb34941d8769713d28a42
Vitória Luzia Nogarol Lopes <44072278858@info.local>
2023-03-31 17:07:34 -0300

    aula 31.03

---
+++ backend-1/atividades-condicionais/extras/exercicio-1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Escreva um programa que pergunte o dia, mês e ano do aniversário de uma pessoa e diga se a data é válida ou não. Caso não seja, diga o motivo. Suponha que todos os meses tem 31 dias e que estejamos no ano de 2013.
+
+Console.WriteLine($"Informe o dia de seu nascimento:");
+int dia = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o mes de seu nascimento:");
+int mes = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o ano de seu nascimento:");
+int ano = int.Parse(Console.ReadLine());
+
+
+if (dia > 31 && dia < 1)
+{
+    Console.WriteLine($"Dia invalida");
+
+    if (mes > 12 && mes < 1)
+    {
+        Console.WriteLine($"Mes Invalida");
+    }
+
+    else if (ano > 2013 && ano < 1)
+    {
+        Console.WriteLine($"Ano invalida");
+    }
+
+}
+
+else
+{
+    Console.WriteLine($"data invalida");
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ backend-1/atividades-condicionais/extras/exercicio-2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Faça um programa que leia três números e mostre o maior e o menor deles.
+
+Console.WriteLine($"Informe o primeiro numero:");
+float n1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o segundo numero:");
+float n2 = float.Parse(Console.ReadLine());
+
+if (n1 > n2){
+    Console.WriteLine($"{n1} é o maior");
+    
+}
+
+else {
+    Console.WriteLine($"{n2} é o maior");
+    
+}
