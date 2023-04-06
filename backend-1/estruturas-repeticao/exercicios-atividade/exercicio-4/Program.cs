01ae1ea636941f6975a8c3805e28c9d6ff2cfb1c
Vitória Luzia Nogarol Lopes <44072278858@info.local>
2023-04-06 17:00:23 -0300

    vs. 06/04

--- backend-1/estruturas-repeticao/exercicios-atividade/exercicio-4/Program.cs
+++ backend-1/estruturas-repeticao/exercicios-atividade/exercicio-4/Program.cs
@@ -5,11 +5,41 @@
 // C.O número de mulheres que responderam SIM;
 // D.A porcentagem de homens que responderam NÃO entre todos os homens analisados.
 
-int formula, contador;
-Console.WriteLine($"Qual seu genero? Insira s para sim, e n para nao.");
-string genero = Console.ReadLine();
 
-Console.WriteLine($"");
+int pessoasS = 0;
+int pessoasN = 0;
+int femS = 0; 
+int mascN = 0;
 
+for ( int contador = 1; contador <= 10; contador ++)
+{
+    Console.WriteLine($"Qual seu genero? Insira m para masculino, e f para feminino.");
+    char genero = char.Parse(Console.ReadLine());
 
+    Console.WriteLine($"Gostou do produto lançado? Insira s para sim, e n para não.");
+    char produto = char.Parse(Console.ReadLine());
 
+    switch (produto)
+    {
+        case 's':
+        pessoasS ++;
+
+        if (genero == 'f' ) {
+            femS ++;
+        }
+        break;
+
+        case 'n':
+        pessoasN ++;
+
+        if (genero == 'm') {
+            mascN ++;
+        }
+        break;
+
+        // default:
+    }
+  
+}
+
+    Console.WriteLine($"O numero de pessoas que responderam sim, foi: {pessoasS}. O numero de pessoas que responderam nao, foi: {pessoasN}. Numero de mulheres que responderam sim, foi: {femS}. E a porcentagem de homens que responderam nao, foi {mascN }");
