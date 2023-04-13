9af808b531a004a2e377e19f752efc35dab43ce4
Vitória Luzia Nogarol Lopes <44072278858@info.local>
2023-04-13 14:21:54 -0300

    Funcao

--- backend-1/arrays/exercicio-fixacao/Program.cs
+++ backend-1/arrays/exercicio-fixacao/Program.cs
@@ -2,16 +2,16 @@
 //construir dois arrays para nome e idade 
 // construir a pergunta para nome e idade
 
-
+int[] idade = new int[5];
 string[] nome = new string[5];
-string[] idade = new string[5];
 
-for (int i = 0; i < 5; i++)
+
+for (var i = 0; i < 5; i++)
 {
     Console.Write($"Digite o {i + 1}º nome: ");
     nome[i] = (Console.ReadLine());
     Console.Write($"Digite a {i + 1}º idade: ");
-    idade[i] = (Console.ReadLine());
+    idade[i] = int.Parse (Console.ReadLine());
 }
 
 
@@ -20,6 +20,6 @@ for (int i = 0; i < 5; i++)
     Console.ForegroundColor = ConsoleColor.Blue;
     Console.WriteLine($" {i + 1}) Nome: {nome[i]}");
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"{i + 1}) Idade: {idade[i]}");
+    Console.WriteLine($" Idade: {idade[i]}");
 }
 
---
+++ backend-1/funcao/Program.cs
@@ -0,0 +1,91 @@
+﻿// // criar um metodo para multiplicar 2 numeros 
+
+// static float Multiplicar (float n1, float n2) {
+//     float m = n1 * n2;
+//     return m;
+// }
+
+// float resultadoM = Multiplicar(4, 8);
+// Console.WriteLine($"Resultado {resultadoM}");
+
+// //criar um metodo para dividir 2 numeros 
+
+// static float Dividir(float n1, float n2)
+// {
+//     float d = n1 / n2;
+//     return d;
+// }
+
+// float resultadoD = Dividir(9, 10);
+// Console.WriteLine($"Resultado {resultadoD}");
+
+// //criar um metodo para subtrair 2 numeros 
+
+// static float Subtrair(float n1, float n2)
+// {
+//     float s = n1 - n2;
+//     return s;
+// }
+
+// float resultadoS = Subtrair(32, 8);
+// Console.WriteLine($"Resultado {resultadoD}");
+
+//************************************************************************
+
+//crie um metodo para multiplicar 2 numeros 
+static float Multiplicar(float n1, float n2)
+{
+    return (n1 * n2);
+}
+
+Console.WriteLine($"Digite o primeiro numero para multiplicar");
+float n1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite o segundo numero para multiplicar");
+float n2 = float.Parse(Console.ReadLine());
+
+
+float resultadoM = Multiplicar(n1, n2);
+Console.WriteLine($"Resultado {resultadoM}");
+
+
+
+
+
+
+//crie um metodo para dividir 2 numeros
+
+static float Dividir(float n1, float n2)
+{
+    return (n1 / n2);
+}
+
+Console.WriteLine($"Digite o primeiro numero para dividir");
+float dividir1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite o segundo numero para dividir");
+float dividir2 = float.Parse(Console.ReadLine());
+
+
+float resultadoD = Dividir(dividir1, dividir2);
+Console.WriteLine($"Resultado {resultadoD}");
+
+
+
+//crie um metodo para subtrair 2 numeros 
+
+
+static float Subtrair(float n1, float n2)
+{
+    return (n1 - n2);
+}
+
+Console.WriteLine($"Digite o primeiro numero para subtrair");
+float subtrair1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite o segundo numero para subtrair");
+float subtrair2 = float.Parse(Console.ReadLine());
+
+
+float resultadoS = Subtrair(subtrair1, subtrair2);
+Console.WriteLine($"Resultado {resultadoS}");
